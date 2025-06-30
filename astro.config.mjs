deb98696502bc2ec72c39eed1d85de869243beb2
heimdall134 <heimdall134@duck.com>
2025-06-30 18:04:46 +0200

    added files

---
+++ astro.config.mjs
@@ -0,0 +1,15 @@
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import icon from "astro-icon";
+
+// https://astro.build/config
+export default defineConfig({
+    // site: "https://ambientesconarte.es",
+    site: "https://wonderful-starburst-2a2deb.netlify.app",
+    integrations: [mdx(), sitemap(), icon()],
+    vite: {
+        plugins: [tailwindcss()],
+    },
+});
